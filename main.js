7c024eee72e977314d836bdbbf21e16e80547a7a
probablynotgrady <ipodidiot1000@gmail.com>
2025-11-19 17:27:04 +0000

    added a bunch of files and still cant draw an image -Chase

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /* Main game file: main.js */
-/* Game: [Your Game Name Here] */
-/* Authors: [Your Name(s) Here] */
+/* Game: DVD Logo Simulator */
+/* Authors: Avalynn Annarelli, Chase DeLuca */
 /* Description: [Short description of your game here] */
 /* Citations: [List any resources, libraries, tutorials, etc you used here] 
 /* Note: If you use significant AI help you should cite that here as well */
@@ -10,43 +10,36 @@
 /* auto-complete it maye be impractical to mark every line, which is why you */
 /* should also include a summary here */
 
-
 import "./style.css";
 
-import { GameInterface } from 'simple-canvas-library';
+import { GameInterface } from "simple-canvas-library";
 
 let gi = new GameInterface();
 
 /* Variables: Top-Level variables defined here are used to hold game state */
 
-
 /* Drawing Functions */
+const dvdImg = new Image();
+dvdImg.src = new URL('./DVD.png', import.meta.url).href;
 
 /* Example drawing function: you can add multiple drawing functions
 that will be called in sequence each frame. It's a good idea to do 
 one function per each object you are putting on screen, and you
 may then want to break your drawing function down into sub-functions
 to make it easier to read/follow */
-gi.addDrawing(
-  function ({ ctx, width, height, elapsed, stepTime }) {
-    // Your drawing code here...    
-  }
-)
+gi.addDrawing(function ({ ctx, width, height, elapsed, stepTime }) {
+  // draw dvd logo at random position
+  ctx.drawImage(dvdImg, 0, 0, 100, 50);
+});
 
 /* Input Handlers */
 
 /* Example: Mouse click handler (you can change to handle 
 any type of event -- keydown, mousemove, etc) */
 
-gi.addEventListener(
-  "click",
-  function ({ event, x, y }) {
-    // Your click handling code here...
-  }
-)
-
+gi.addEventListener("click", function ({ event, x, y }) {
+  //
+});
 
 /* Run the game */
 gi.run();
-
-
